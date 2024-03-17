f23df037fc6ab85947ca27bda4a5dd5756400e9c
Malte Ubl <malte.ubl@gmail.com>
2024-03-17 12:30:29 -0700

    Add a matcher

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,19 @@
 import { get } from "@vercel/edge-config";
 import { NextRequest, NextResponse } from "next/server";
 
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
+
 // Configuration stored in Edge Config.
 interface BlueGreenConfig {
   deploymentDomainBlue: string;
